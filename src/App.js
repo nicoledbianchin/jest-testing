53a5003b56df0dd295cf4624757c96ed803bce45
nicoledbianchin <nicoledbianchin@gmail.com>
2020-09-16 23:57:21 -0300

    :lipstick: adjust input and title styles

--- src/App.js
+++ src/App.js
@@ -13,62 +13,38 @@ import {
 const App = () => {
   return (
     <>
-    <View style={styles.titleContainer}>
+    <View style={styles.container}>
       <Text style={styles.title} testID={'rectangleLengthText'}>Length</Text>
-      <Text style={styles.title} testID={'rectangleWidthText'}>Width</Text>
+      <TextInput style={styles.input} testID={'rectangleLengthInput'}/>
     </View>
-    <TextInput testID={'rectangleLengthInput'}/>
-    <TextInput testID={'rectangleWidthInput'}/>
+    <View style={styles.container}>
+      <Text style={styles.title} testID={'rectangleWidthText'}>Width</Text>
+      <TextInput style={styles.input} testID={'rectangleWidthInput'}/>
+    </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-  titleContainer: {
+  container: {
     flexDirection: 'row',
-    justifyContent: 'space-around'
+    justifyContent: 'space-between'
   },
   title: {
     fontSize: 24,
     fontWeight: '600',
     color: Colors.black,
-    marginVertical: 60
+    marginTop: 60,
+    marginBottom: 20,
+    marginHorizontal: 60
+  },
+  input: {
+    borderColor: Colors.blue,
+    borderWidth: 1,
+    marginTop: 66,
+    marginRight: 60,
+    width: 100,
+    height: 20
   }
 });
 
