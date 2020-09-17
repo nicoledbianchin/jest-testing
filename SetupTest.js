b0515de30b682033a91b4bf1c483e97300abbec2
nicoledbianchin <nicoledbianchin@gmail.com>
2020-09-16 22:42:19 -0300

    :wrench: setup configuration for jest testing

---
+++ SetupTest.js
@@ -0,0 +1,15 @@
+import 'react-native'
+import 'react-native-mock-render/mock'
+import { JSDOM } from 'jsdom'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+global.Document = new JSDOM()
+global.window = document.defaultView
+Object.keys(document.defaultView).forEach(property => {
+    if (typeof global[property] === 'undefined') {
+        global[property] = document.defaultView[property]
+    }
+})
+
+configure({adapter: new Adapter()})
